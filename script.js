91fe79a2787851127e4e1bb369897a1d5f5f4e27
Aksh Developer <akshaykumaraditya01@gmail.com>
2026-01-08 15:40:26 +0530

    created all file and code

---
+++ script.js
@@ -0,0 +1,15 @@
+const mouseTracker = document.querySelector(".mouse-tracker");
+const result = document.querySelector(".result");
+
+window.addEventListener("mousemove", (e) => {
+    let left = 0;
+    let top = 0;
+
+    left = e.pageX;
+    top = e.pageY;  
+
+    result.innerText = `leftX: ${left} TopY: ${top}`;
+    
+    mouseTracker.style.left = left + "px";
+    mouseTracker.style.top = top + "px";
+});
